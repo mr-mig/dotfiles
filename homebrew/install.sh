f2c079ee0a3cd21cbe52cedda3229c32b64158c3
Alex Migutsky <mr-mig@github.com>
2021-10-27 19:29:26 +0200

    Replace homebrew ruby script with bash version

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -4,7 +4,7 @@ if [ $UNIX ]
 then
   # Check for Homebrew and install if we don't have it
   if test ! $(which brew); then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 
   # Update Homebrew recipes
